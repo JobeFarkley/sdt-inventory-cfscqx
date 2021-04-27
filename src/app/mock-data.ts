b68be27212f79e35aa667f9ee832cba4de4ffc8b
Drednite <drednite@live.com>
2021-04-26 21:07:49 -0500

    1 file modified

---
+++ src/app/mock-data.ts
@@ -0,0 +1,80 @@
+export let USERS: any = [
+  {
+    UID: 523645,
+    username: "Samuel.Dailey",
+    password: "thereisnospoon",
+    permissions: "ITEmp"
+  },
+  {
+    UID: 877530,
+    username: "Dalin.Christianson",
+    password: "",
+    permissions: ""
+  },
+  {
+    UID: 225549,
+    username: "Matt.Wilson",
+    password: "",
+    permissions: ""
+  },
+  {
+    UID: 189023,
+    username: "Kole.Dorbor",
+    password: "",
+    permissions: ""
+  }
+];
+
+export let DEVICES: any = [
+  {
+    DID: 10536,
+    deviceName: "PC Workstation #1",
+    assignedUID: 523645
+  },
+  {
+    DID: 36525,
+    deviceName: "PC Workstation #2",
+    assignedUID: 636598
+  },
+  {
+    DID: 56895,
+    deviceName: "Workstation Tablet",
+    assignedUID: 556789
+  },
+  {
+    DID: 85478,
+    deviceName: "Mobile Workstation",
+    assignedUID: 357895
+  }
+];
+
+export let REPAIRS: any = [
+  {
+    RID: 25368,
+    DID: 36525,
+    issue: "Won't turn on.",
+    fix: "Plugged in",
+    status: "Resolved"
+  },
+  {
+    RID: 56985,
+    DID: 56895,
+    issue: "Battery lasts less than 10 min off charger",
+    fix: "Uninstalled non-work apps.",
+    status: "Monitoring"
+  },
+  {
+    RID: 48569,
+    DID: 85478,
+    issue: "Trackpad not responding",
+    fix: "",
+    status: "Pending"
+  },
+  {
+    RID: 24956,
+    DID: 36525,
+    issue: "Keeps bothering me to update",
+    fix: "Restarted PC",
+    status: "Resolved"
+  }
+];
