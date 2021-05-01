1eb0239bb0cb7da19d4f733fa0f45defe5b620c0
JobeFarkley <dalin.christianson@yahoo.com>
2021-05-01 18:53:53 -0500

    6 files modified

--- src/app/repair-request/repair-request.component.ts
+++ src/app/repair-request/repair-request.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepairRequestComponent implements OnInit {
 
+  // myInput0 = ""
+  // myInput1 = ""
+  // myInput2 = ""
+  // myInput3 = ""
+  // myInput4 = ""
+
+  // submitWorkOrder() {
+  //   WORK[0].name = this.myInput0;
+  //   WORK[1].empID = this.myInput1;
+  //   WORK[2].email = this.myInput2;
+  //   WORK[3].location = this.myInput3;
+  //   WORK[4].description = this.myInput4;
+  
+  // }
   constructor() { }
 
   ngOnInit() {
